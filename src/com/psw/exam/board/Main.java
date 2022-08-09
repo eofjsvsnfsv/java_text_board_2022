d1e4625620ba4743766321c3e671d5e2b3d81809
eofjsvsnfsv <ghtn1412@naver.com>
2022-08-09 15:43:12 +0900

    세팅

---
+++ src/com/psw/exam/board/Main.java
@@ -0,0 +1,10 @@
+package com.psw.exam.board;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("====게시판 v 0.1 ====");
+    System.out.println("====게시판 v 0.1 ====");
+
+
+  }
+}
